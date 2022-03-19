64105176fdee5c95e9a1ebced0e3863d9c68af7c
tadeu-p <101936207+tadeu-p@users.noreply.github.com>
2022-03-19 17:08:16 -0300

    Update cptohost.sh

--- cptohost.sh
+++ cptohost.sh
@@ -1,71 +1,32 @@
 #!/bin/bash
 
-# SENHA ROOT
-senha='8h2y3a'
+# ROOT PASSWORD
+password='******'
 
 # FILE PATH ON THE SOURCE PC
-file14='/home/POP-01.27.14-guia-escolha-tratamento-antibacteriano-endovenoso-principais-infeccoes-adulto.pdf'
-file15='/home/POP-01.27.15-profilaxia-antimicrobiana-procedimentos-cirurgicos.pdf'
+file14='/home/FILE-NAME14'
+file15='/home/FILE-NAME15'
 
 # SCRIPT TO BE RUN
 script='/usr/local/bin/mvtodesktop.sh'
 
-
-# OS IPS NÃO COMENTADOS SÃO OS QUE ESTÃO FALTANDO
-
 ip14=(
-	#PS
-	# "192.168.3.48" 
-	# "192.168.0.145" 
-	# "192.168.1.134" 
-	# "192.168.3.51" 
-	# "192.168.3.52" 
-	# "192.168.1.129" 
-	# "192.168.0.252" 
-	# "192.168.1.19" 
-	# "192.168.0.186" 
-	# "192.168.3.55"
-	#CM
-	# "192.168.3.28"
-	# "192.168.3.29"
-	# "192.168.3.31"
-	# "192.168.3.32"
-	# "192.168.3.251"
-	# "192.168.3.252"
-	# "192.168.3.253"
-	# "192.168.0.171"
-	# "192.168.0.172"
+	"192.168.*.*" 
+	"192.168.*.*"
+	"192.168.*.*"
+	"192.168.*.*"
+	"192.168.*.*"
+	"192.168.*.*"
 	)
 ip15=(
-	#CC
-	# "192.168.3.33"
-	# "192.168.3.34"
-	# "192.168.3.50"
-	# "192.168.3.62"
-	"192.168.3.78"
-	# "192.168.1.209"
-	# "192.168.3.84"
-	#NEURO
-	"192.168.3.88"
-	# "192.168.3.66"
-	# "192.168.3.43"
-	# "192.168.3.44"
-	# "192.168.0.147"
-	# "192.168.0.148"
-	# "192.168.0.149"
-	# "192.168.0.150"
-	"192.168.0.151"
-	#PA
-	# "192.168.0.206"
-	# "192.168.0.204"
-	# "192.168.3.67"
-	# "192.168.0.205"
-	# #PB
-	# "192.168.0.237"
-	# "192.168.3.58"
-	# "192.168.0.251"
-	# "192.168.3.59"
-	# "192.168.3.69"
+	"192.168.*.*"
+	"192.168.*.*"
+	"192.168.*.*"
+	"192.168.*.*"
+	"192.168.*.*"
+	"192.168.*.*"
+	"192.168.*.*"
+	"192.168.*.*"
 	)
 
 function cptohost() {
@@ -77,7 +38,7 @@ for i in "${!ip[@]}";
 		if
 			ping -c1 -q "${ip[i]}" &>/dev/null
 			then
-				sshpass -p $senha scp -o StrictHostKeyChecking=no "$file" root@"${ip[i]}":/tmp/;
+				sshpass -p $password scp -o StrictHostKeyChecking=no "$file" root@"${ip[i]}":/tmp/;
 				printf 'FILE COPIED TO: '"${ip[i]}"' ✓\n'
 		else
 			 printf 'HOST '"${ip[i]}"' IS OFFLINE ✗\n'
@@ -94,7 +55,7 @@ for i in "${!ip[@]}";
 		if
 			ping -c1 -q "${ip[i]}" &>/dev/null
 			then
-				sshpass -p $senha ssh root@"${ip[i]}"	'bash -s' < $script;
+				sshpass -p $password ssh root@"${ip[i]}"	'bash -s' < $script;
 		else
 			printf 'HOST '"${ip[i]}"' IS OFFLINE ✗\n'
   		fi
@@ -102,9 +63,9 @@ done
 	}
 
 #######  CP TO HOST - POP 14  #######
-# cptohost $file14 "${ip14[@]}"
+cptohost $file14 "${ip14[@]}"
 #######  MV ON HOST - POP 14  #######
-# mvtodesktop $file14 "${ip14[@]}"
+mvtodesktop $file14 "${ip14[@]}"
 
 #######  CP TO HOST - POP 15  #######
 cptohost $file15 "${ip15[@]}"
